129218e323252abc12782918de941c5950cde2ab
hialdev <al.developer12@gmail.com>
2021-05-19 13:49:04 +0700

    update

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,27 @@
 // Components
 //-------------------------------
 
+//Hero Slider
+$('.slider-box').owlCarousel({
+    autoWidth:true,
+    center:true,
+    loop:true,
+    margin:1,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+
 //------ Header
 var header = $('.header');
 var textDropdown = $('nav.menu ul li.dropdown ul li a');

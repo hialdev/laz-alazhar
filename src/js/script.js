bfaa329ab155759023ee7990301813a6850ae20d
hialdev <al.developer12@gmail.com>
2021-09-02 22:23:39 +0700

    add lightbox calculator

--- src/js/script.js
+++ src/js/script.js
@@ -158,6 +158,51 @@ calcZiswaf.find('fieldset input').keyup(function(e) {
     var idr = rupiah(val);
     $('.ziswaf-hitung span#idr').text(idr);
 });
+
+// var rupiah = document.querySelector('input#idr');
+// rupiah.addEventListener('keyup', function(e){
+//     // tambahkan 'Rp.' pada saat form di ketik
+//     // gunakan fungsi formatRupiah() untuk mengubah angka yang di ketik menjadi format angka
+//     rupiah.value = formatRupiah(this.value, 'Rp. ');
+// });
+
+// /* Fungsi formatRupiah */
+// function formatRupiah(angka, prefix){
+//     var number_string = angka.replace(/[^,\d]/g, '').toString(),
+//     split   		= number_string.split(','),
+//     sisa     		= split[0].length % 3,
+//     rupiah     		= split[0].substr(0, sisa),
+//     ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
+
+//     // tambahkan titik jika yang di input sudah menjadi angka ribuan
+//     if(ribuan){
+//         separator = sisa ? '.' : '';
+//         rupiah += separator + ribuan.join('.');
+//     }
+
+//     rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+//     return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+// }
+//-------- Lightbox Calculator Zakat
+var test = $('#ctg-zakat');
+var lbCalc = $('.lightbox-calc');
+var lbCalcX = $('.lightbox-calc .box .head .close');
+var btnClac = $('#kalkulator');
+
+test.change(function(){
+    lbCalc.find('box').hide();
+    var i = $(this).children("option:selected").val();
+    
+    return i;
+});
+btnClac.click(function(){
+    lbCalc.css('display','flex');
+    lbCalc.find('box#'+i).show();
+});
+lbCalcX.click(function(){
+    lbCalc.fadeOut(200);
+});
+
 //-------------------------------
 // End Components
 //-------------------------------
